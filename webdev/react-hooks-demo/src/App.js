f23184e7da5700e5bab6dabdd6b83044b2a91347
neural-navigator <iamsatyapati@gmail.com>
2024-07-16 12:00:00 +0530

    Added react-hooks-demo on 2024-07-16T12:00:00

---
+++ webdev/react-hooks-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter1 from "./components/customHook/Counter1";
+
+function App() {
+  
+  return (
+      <div>
+        <Counter1 />
+      </div>
+  );
+}
+
+export default App;
