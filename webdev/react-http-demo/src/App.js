77ac683a6bc7bf15461b1fd3735c5744f5885bbd
neural-navigator <iamsatyapati@gmail.com>
2024-07-17 12:00:00 +0530

    Added react-http-demo on 2024-07-17T12:00:00

---
+++ webdev/react-http-demo/src/App.js
@@ -0,0 +1,13 @@
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+
+function App() {  
+  return (
+    <div>
+      {/* <PostList /> */}
+      <PostForm />
+    </div>
+  );
+}
+
+export default App;
